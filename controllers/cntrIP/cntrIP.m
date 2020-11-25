e822f0852d1f9c237bc4448270bd6207316ebcde
Josef Walek <63097858+robobo789@users.noreply.github.com>
2020-11-25 16:16:14 +0100

    Initiated motors

--- controllers/cntrIP/cntrIP.m
+++ controllers/cntrIP/cntrIP.m
@@ -1,51 +1,21 @@
-% MATLAB controller for Webots
-% File:          cntrIP.m
-% Date:
-% Description:
-% Author:
-% Modifications:
-
-% uncomment the next two lines if you want to use
-% MATLAB's desktop to interact with the controller:
-%desktop;
-%keyboard;
-
 TIME_STEP = 64;
 
-% get and enable devices, e.g.:
-%  camera = wb_robot_get_device('camera');
-%  wb_camera_enable(camera, TIME_STEP);
-%  motor = wb_robot_get_device('motor');
-
-  WbDeviceTag wheels[4];
-  char wheels_names[4][8] = {"wheel1", "wheel2", "wheel3", "wheel4"};
-  for (i = 0; i < 4; i++) {
-    wheels[i] = wb_robot_get_device(wheels_names[i]);
-    wb_motor_set_position(wheels[i], INFINITY);
-  }
-  
-    wb_motor_set_velocity(wheels[0], left_speed);
-    wb_motor_set_velocity(wheels[1], right_speed);
-    wb_motor_set_velocity(wheels[2], left_speed);
-    wb_motor_set_velocity(wheels[3], right_speed);
-
-% main loop:
-% perform simulation steps of TIME_STEP milliseconds
-% and leave the loop when Webots signals the termination
-%
-while wb_robot_step(TIME_STEP) ~= -1
-
-  % read the sensors, e.g.:
-  %  rgb = wb_camera_get_image(camera);
-
-  % Process here sensor data, images, etc.
-
-  % send actuator commands, e.g.:
-  %  wb_motor_set_postion(motor, 10.0);
-
-  % if your code plots some graphics, it needs to flushed like this:
-  drawnow;
-
+wheels = [];
+wheels_names = [ "wheel1", "wheel2", "wheel3", "wheel4" ];
+for i = 1:4
+  wheels(i) = wb_robot_get_device(convertStringsToChars(wheels_names(i)));
+  wb_motor_set_position(wheels(i), inf);
+  wb_motor_set_velocity(wheels(i), 0.0);
 end
 
-% cleanup code goes here: write data to files, etc.
+while wb_robot_step(TIME_STEP) ~= -1
+  left_speed = 5.0;
+  right_speed = 2.5;
+  
+  wb_motor_set_velocity(wheels(1), left_speed);
+  wb_motor_set_velocity(wheels(2), right_speed);
+  wb_motor_set_velocity(wheels(3), left_speed);
+  wb_motor_set_velocity(wheels(4), right_speed);
+  % if your code plots some graphics, it needs to flushed like this:
+  drawnow;
+end
