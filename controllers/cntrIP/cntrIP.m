0dddeb65b9f8a419791a8f344ef688a019e077f2
Josef Walek <63097858+robobo789@users.noreply.github.com>
2020-11-29 22:07:26 +0100

    Angled wheels

--- controllers/cntrIP/cntrIP.m
+++ controllers/cntrIP/cntrIP.m
@@ -10,38 +10,44 @@ for i = 1:3
 end
 
 
-pitchXPID = PID(100,10,10);
+pitchXPID = PID(50,10,5);
 pitchXPID.enable();
 desired_pitchX = 0;
 prev_desired_pitchX = 0;
 
-pitchYPID = PID(100,10,10);
+pitchYPID = PID(50,10,5);
 pitchYPID.enable();
 desired_pitchY = 0;
 prev_desired_pitchY = 0;
 
+sample_setpointX = 0;
+sample_positionX = 0;
+sample_setpointY = 0;
+sample_positionY = 0;
 
 velocity = 1;
 speed = [0,0,0];
 heading = 0;
 
+
+fig = figure();
 while wb_robot_step(TIME_STEP) ~= -1
     
     
     pitch_roll_yaw = wb_inertial_unit_get_roll_pitch_yaw(IMU)
-
-    pitchXPID.update(pitch_roll_yaw(1),0.-0.001)
+    
+    pitchXPID.update(pitch_roll_yaw(1),0.0005)
     desired_xspeed = pitchXPID.output
-
+    
     pitchYPID.update(pitch_roll_yaw(2),0)
     desired_yspeed = pitchYPID.output
     
     velocity = norm([desired_xspeed,desired_yspeed])
     
     if desired_xspeed > 0;
-      heading = asin(desired_yspeed/velocity)+pi;
+        heading = asin(desired_yspeed/velocity)+pi;
     else
-      heading = asin(desired_yspeed/velocity);
+        heading = asin(desired_yspeed/velocity);
     end
     
     
@@ -53,5 +59,30 @@ while wb_robot_step(TIME_STEP) ~= -1
         motors(i).run(speed(i));
     end
     
-    drawnow;
+    
+    fig = subplot(2,1,1);
+    sample_setpointX = [sample_setpointX, pitchXPID.setpoint];
+    sample_positionX = [sample_positionX, pitchXPID.input];
+    
+    plot3(sample_setpointX, "g-")
+    hold on
+    plot3(sample_positionX, "b-");
+    grid on
+    axis([0 inf -0.1 0.1]);
+    hold off
+    
+    sample_setpointY = [sample_setpointY, pitchYPID.setpoint];
+    sample_positionY = [sample_positionY, pitchYPID.input];
+    
+    
+    fig = subplot(2,1,2);
+    plot3(sample_setpointY, "g-")
+    hold on
+    plot3(sample_positionY, "b-");
+    grid on
+    axis([0 inf -0.1 0.1]);
+    hold off
+    
+
+drawnow;
 end
